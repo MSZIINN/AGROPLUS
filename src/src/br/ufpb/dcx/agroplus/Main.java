0a2d97db1a635a41ceddde711f1c2f51f25cd8d1
MARIANO SANTOS <marianosantosmme@gmail.com>
2026-09-08 14:36:21 -0300

    Adiciona classe Main do AgroPlus

---
+++ src/src/br/ufpb/dcx/agroplus/Main.java
@@ -0,0 +1,15 @@
+package br.ufpb.dcx.agroplus;
+
+import br.ufpb.dcx.agroplus.view.ConsoleView;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleView app = new ConsoleView();
+        app.iniciar();
+    }
+}
+
+
+// E-mail: admin@agroplus.com
+//Senha: admin123
+// cada trabalhador vai ter o seu
